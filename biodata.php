b64138921819c6ab63e09157abc8943583cc05cf
novalhd7 <109157618+novalhd7@users.noreply.github.com>
2023-10-07 02:25:45 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <title>Biodata</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <header>
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <h1 class="navbar-brand  fa-align-center">
+      <img src="gamba.jpg" alt="Logo" width="100" height="100" class="d-inline-block align-text-top">
+      Biodata
+    </h1>
+  </div>
+</nav>
+    </header>
+    <div>
+        <h2><i class="fa fa-align-center" aria-hidden="true">Noval Hidayat</i></h2>
+        <p>saya berkuliah di: </b> Universitas Andalas<br>
+           Jurusan: </b> Teknik komputer <br>
+           saya ikut program studi independent full stack di PT GITS indonesia untuk melatih skill saya di bidang website</p>
+    </div>
+   
+</body>
